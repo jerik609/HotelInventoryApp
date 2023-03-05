763d02dbcfe396f36640305bd0c5f73605d3a78b
Michal <117671671+jerik609@users.noreply.github.com>
2023-03-05 19:16:34 +0100

    exploring angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-@NgModule({
-  declarations: [
+// https://angular.io/guide/ngmodules
+// I can have any number of modules in my application, but there always has to be only one root module
+// the ngmodule decorator modifies the behavior of the class - it (injects?) the information about all the components, modules, etc in our application, which we want to use
+// basically, we configure this module as an root module (not saying it's a root module, but we set it up in such a way, that it can be used as a root module)
+// this configuration is passed to the compiler
+// then we can bootstrap this module in main.ts
+
+// NOTE: there are two "bootstraps" we're talking about here:
+// - the module bootstrap in main.ts - defining the root module
+// - the component bootstrap in @NgModule - defining which component will be the root component
+@NgModule({ // annotation marking the class (AppModule) as module
+  declarations: [ // any components, directives and pipes must be registered here
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+  imports: [ // any module we intend to use, must be placed here (both external and those created by us)
+    BrowserModule, // angular platform provided module, part of the browser library
+    AppRoutingModule // approuting module is created by us
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] // this defines, which component will be loaded first (the root component)
 })
-export class AppModule { }
+export class AppModule { } // the root module of the application
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-
+// here we bootstrap the application - setting AppModule as the root module
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
