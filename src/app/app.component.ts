c65ef7de8f4ea44844bc0e37c83235e2bb21ba32
Michal <117671671+jerik609@users.noreply.github.com>
2023-03-08 19:04:15 +0100

    dynamic component creation and any-tag-access using viewchild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { RoomsComponent } from './rooms/rooms.component';
 
 @Component({
   selector: 'app-root', // "name" of the component - this is the name we will use to refer to this component in the html file (index.html)
@@ -12,9 +13,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
   //styles: [`h1 { color: red; }`]
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit, OnInit {
+
   title = 'HotelInventoryApp';
   role = 'Admin';
+
+  // dynamic component creation example
+  @ViewChild("myProperty", { read: ViewContainerRef }) ref?: ViewContainerRef;
+  
+  ngAfterViewInit(): void {
+    let component = this.ref?.createComponent(RoomsComponent);
+    component!.instance.hotelName = "No-Tell Motel";
+  }
+
+  // accessing any tag - example
+  @ViewChild("specialProperty", { static: true }) name!: ElementRef;
+  ngOnInit(): void { // we can use onInit, because the reference is static
+    this.name.nativeElement.innerText = "Well, hello there!";
+  }
+
+
 }
 
 // in future, when I need to add more components, this is where they will be placed (and the respective app.component.html)
