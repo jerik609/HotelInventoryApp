aa62872ffc203668332291c1daaf4f5811adb037
Michal <117671671+jerik609@users.noreply.github.com>
2023-03-18 21:40:42 +0100

    change color on mouse hover - using @HostListener

--- src/app/hover.directive.ts
+++ src/app/hover.directive.ts
@@ -1,17 +1,27 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Directive, ElementRef, HostListener, Inject, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appHover]'
 })
 export class HoverDirective implements OnInit {
 
-  color: string = 'red';
+  @Input() appHover: string = 'red';
 
-  constructor(private element: ElementRef) {
+  constructor(private element: ElementRef, @Inject(DOCUMENT) private document: Document) {
     console.log("THE ELEMENT in HOVER", this.element.nativeElement);
   }
   
   ngOnInit(): void {
-    this.element.nativeElement.style.backgroundColor = this.color;
+    this.element.nativeElement.style.backgroundColor = this.appHover;
   }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.element.nativeElement.style.backgroundColor = 'red';
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.element.nativeElement.style.backgroundColor = this.appHover;
+  }
+
 }
