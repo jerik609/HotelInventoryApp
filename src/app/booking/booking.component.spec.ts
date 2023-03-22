a5e231a7c906c45c5f4fe4fc6ba7df1add1fc4dc
Michal <117671671+jerik609@users.noreply.github.com>
2023-03-22 22:13:00 +0100

    fixing some unit test errors

--- src/app/booking/booking.component.spec.ts
+++ src/app/booking/booking.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookingComponent } from './booking.component';
@@ -8,6 +9,7 @@ describe('BookingComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ HttpClientModule ],
       declarations: [ BookingComponent ]
     })
     .compileComponents();
--- src/app/init.service.spec.ts
+++ src/app/init.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { InitService } from './init.service';
@@ -6,7 +7,9 @@ describe('InitService', () => {
   let service: InitService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientModule ]
+    });
     service = TestBed.inject(InitService);
   });
 
